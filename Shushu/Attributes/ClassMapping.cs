378998a0fe2e3f9e8cadcad2f9f4a7e3fe34ab4b
Joey Frohikey <frohikey@gmail.com>
2017-09-21 17:08:50 +0200

    Add class mapping

---
+++ Shushu/Attributes/ClassMapping.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Shushu.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+    public class ClassMapping : Attribute
+    {
+        public Enums.IndexField IndexField { get; }
+        public object Value { get; }
+
+        public ClassMapping(Enums.IndexField indexField, object value)
+        {            
+            IndexField = indexField;
+            Value = value;
+        }
+    }
+}
---
+++ Shushu/Attributes/PropertyMapping.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Shushu.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public class PropertyMapping : Attribute
+    {
+        public Enums.IndexField IndexField { get; }
+
+        public PropertyMapping(Enums.IndexField indexField)
+        {
+            IndexField = indexField;
+        }
+    }
+}
