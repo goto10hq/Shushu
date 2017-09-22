7062b73dbc5cf4c12a5eddbcd92ec843aa97df55
Joey Frohikey <frohikey@gmail.com>
2017-09-22 10:26:26 +0200

    Remove lock

--- Shushu/Mapper.cs
+++ Shushu/Mapper.cs
@@ -25,94 +25,89 @@ namespace Shushu
         }
     }
 
-    // TODO: checkups for duplicated fields
     public static class Mapper
     {
-        static readonly object _lock = new object();
-
         public static AzureSearch MapToSearch<T>(this T obj) where T : class
         {
             if (obj == null)
                 throw new ArgumentNullException(nameof(obj));
 
-            lock (_lock)
-            {
-                // get class mappings
-                var classMappings = MapperCore.Instance.ClassCache.Get(obj.GetType()) as List<ClassMapping>;
+            // get class mappings
+            var classMappings = MapperCore.Instance.ClassCache.Get(obj.GetType()) as List<ClassMapping>;
 
-                if (classMappings == null)
+            if (classMappings == null)
+            {
+                var cac = obj.GetType().GetCustomAttributes(false);
+                classMappings = new List<ClassMapping>();
+
+                foreach (var ca in cac)
                 {
-                    var cac = obj.GetType().GetCustomAttributes(false);
-                    classMappings = new List<ClassMapping>();
+                    if (ca is ClassMapping ca2)
+                    {
+                        classMappings.Add(ca2);
+                    }
+                }
+
+                MapperCore.Instance.ClassCache.Set(obj.GetType(), classMappings);
+            }
+
+            // get properties mappins
+            var propsMappings = MapperCore.Instance.ClassCache.Get(obj.GetType()) as List<PropertyMapping>;
+
+            if (propsMappings == null)
+            {
+                var props = obj.GetType().GetProperties();
+                propsMappings = new List<PropertyMapping>();
+
+                foreach (var p in props)
+                {
+                    var cac = p.GetCustomAttributes(false);
 
                     foreach (var ca in cac)
                     {
-                        if (ca is ClassMapping ca2)
+                        if (ca is PropertyMapping pm)
                         {
-                            classMappings.Add(ca2);
+                            pm.Property = p.Name;
+
+                            propsMappings.Add(pm);
                         }
                     }
-
-                    MapperCore.Instance.ClassCache.Set(obj.GetType(), classMappings);
                 }
 
-                // get properties mappins
-                var propsMappings = MapperCore.Instance.ClassCache.Get(obj.GetType()) as List<PropertyMapping>;
 
-                if (propsMappings == null)
+                MapperCore.Instance.PropsCache.Set(obj.GetType(), propsMappings);
+            }
+
+            // create azure search instance
+            var search = new AzureSearch();
+
+            // set class mappings
+            foreach (var cm in classMappings)
+            {
+                search.GetType().GetTypeInfo().GetProperty(cm.IndexField.ToString()).SetValue(search, cm.Value);
+            }
+
+            // set property mappings
+            foreach (var pm in propsMappings)
+            {
+                var value = obj.GetPropertyValue(pm.Property);
+
+                if (value != null)
                 {
-                    var props = obj.GetType().GetProperties();
-                    propsMappings = new List<PropertyMapping>();
-
-                    foreach(var p in props)
+                    if (pm.IndexField == Enums.IndexField.Point0 &&
+                        value is GeoPoint gp)
                     {
-                        var cac = p.GetCustomAttributes(false);
-
-                        foreach(var ca in cac)
-                        {
-                            if (ca is PropertyMapping pm)
-                            {
-                                pm.Property = p.Name;
-
-                                propsMappings.Add(pm);
-                            }
-                        }
+                        value = GeographyPoint.Create(gp.Coordinates[0], gp.Coordinates[1]);
+                        search.GetType().GetTypeInfo().GetProperty(pm.IndexField.ToString()).SetValue(search, value);
                     }
-
-                    MapperCore.Instance.PropsCache.Set(obj.GetType(), propsMappings);
-                }
-
-                // create azure search instance
-                var search = new AzureSearch();
-
-                // set class mappings
-                foreach(var cm in classMappings)
-                {
-                    search.GetType().GetTypeInfo().GetProperty(cm.IndexField.ToString()).SetValue(search, cm.Value);
-                }
-
-                // set property mappings
-                foreach (var pm in propsMappings)
-                {
-                    var value = obj.GetPropertyValue(pm.Property);
-
-                    if (value != null)
+                    else
                     {
-                        if (pm.IndexField == Enums.IndexField.Point0 &&
-                            value is GeoPoint gp)
-                        {                            
-                            value = GeographyPoint.Create(gp.Coordinates[0], gp.Coordinates[1]);
-                            search.GetType().GetTypeInfo().GetProperty(pm.IndexField.ToString()).SetValue(search, value);
-                        }
-                        else
-                        {
-                            search.GetType().GetTypeInfo().GetProperty(pm.IndexField.ToString()).SetValue(search, value);
-                        }
+                        search.GetType().GetTypeInfo().GetProperty(pm.IndexField.ToString()).SetValue(search, value);
                     }
                 }
+            }
 
-                return search;
-            }            
+            return search;
         }
     }
 }
