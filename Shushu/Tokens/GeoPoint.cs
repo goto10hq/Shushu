b8c8953f60ad44b851aadaf6e34cb8c858fd8db5
frohikey <frohikey@gmail.com>
2017-09-23 10:48:22 +0200

    Clean up

--- Shushu/Tokens/GeoPoint.cs
+++ Shushu/Tokens/GeoPoint.cs
@@ -1,8 +1,20 @@
 ﻿namespace Shushu.Tokens
 {
+    /// <summary>
+    /// Geo point.
+    /// </summary>
     public class GeoPoint
     {
+        /// <summary>
+        /// Gets the type.
+        /// </summary>
+        /// <value>The type.</value>
         public string Type => "Point";
+
+        /// <summary>
+        /// Gets or sets the coordinates.
+        /// </summary>
+        /// <value>The coordinates.</value>
         public double[] Coordinates { get; set; }
         
         public GeoPoint()
