0044a402e0387d3196661de571c0f55bdaeee3ba
frohikey <frohikey@gmail.com>
2017-09-23 13:13:42 +0200

    Add indexing batches + merge option

--- Shushu.Tests/Search.cs
+++ Shushu.Tests/Search.cs
@@ -2,6 +2,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Shushu.Attributes;
 using System.IO;
+using System.Collections.Generic;
 
 namespace Shushu.Tests
 {
@@ -46,11 +47,20 @@ namespace Shushu.Tests
 
             _shushu = new Shushu(appSettings["Shushu:Name"], appSettings["Shushu:ServiceApiKey"], appSettings["Shushu:SearchApiKey"], appSettings["Shushu:Index"]);
 
-            var s0 = new Shu("1", "nenecchi", 130);
+            var s0 = new Shu("1", "nene", 130);
             var s1 = new Shu("2", "umiko", 150);
 
             _shushu.IndexDocument(s0);
             _shushu.IndexDocument(s1);
+
+            var shushus = new List<Shu>
+            {
+                new Shu("3", "aoba", 140),
+                new Shu("4", "momiji", 110),
+                new Shu("5", "hifumi", 110),
+            };
+
+            _shushu.IndexDocuments(shushus);
         }
 
         [TestCleanup]
--- Shushu/Shushu.cs
+++ Shushu/Shushu.cs
@@ -13,11 +13,16 @@ namespace Shushu
     /// </summary>
     public class Shushu
     {
+        /// <summary>
+        /// The size of the batch.
+        /// </summary>
+        /// <remarks>Note that you can only include up to 1000 documents (or 16 MB) in a single indexing request</remarks>
+        const int BatchSize = 1000;
+
         SearchServiceClient _serviceClient;
         SearchIndexClient _searchClient;
         SearchIndexClient _indexClient;
-
-        string _index;
+        readonly string _index;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="T:Shushu.Shushu"/> class.
@@ -34,9 +39,9 @@ namespace Shushu
             _searchClient = new SearchIndexClient(name, index, new SearchCredentials(searchApiKey));
             _indexClient = new SearchIndexClient(name, index, new SearchCredentials(serviceApiKey));
 
-            var ind = _serviceClient.Indexes.List().Indexes.FirstOrDefault(x => x.Name.Equals(index));
+            var idx = _serviceClient.Indexes.List().Indexes.FirstOrDefault(x => x.Name.Equals(index));
 
-            if (ind == null)
+            if (idx == null)
                 CreateIndex();            
         }
 
@@ -58,24 +63,72 @@ namespace Shushu
         }
 
         /// <summary>
-        /// Indexs the document.
+        /// Index document.
         /// </summary>
         /// <param name="document">Document.</param>
-        public void IndexDocument<T>(T document) where T: class
+        /// <param name="merge">If set to <c>true</c> merge otherwise just upload.</param>
+        /// <typeparam name="T">The document type.</typeparam>
+        public void IndexDocument<T>(T document, bool merge = true) where T: class
         {
-            AsyncTools.RunSync(() => IndexDocumentAsync(document));
+            AsyncTools.RunSync(() => IndexDocumentAsync(document, merge));
         }
 
         /// <summary>
-        /// Indexs the document.
+        /// Index document.
         /// </summary>
-        /// <returns>The document.</returns>
         /// <param name="document">Document.</param>
-        public async Task IndexDocumentAsync<T>(T document) where T: class
+        /// <param name="merge">If set to <c>true</c> merge otherwise just upload.</param>
+        /// <typeparam name="T">The document type.</typeparam>
+        public async Task IndexDocumentAsync<T>(T document, bool merge = true) where T: class
         {
             var documents = new List<AzureSearch> { document.MapToIndex() };
-            var batch = IndexBatch.Upload(documents);
-            await _indexClient.Documents.IndexAsync(batch);
+
+            if (merge)
+            {
+                var batch = IndexBatch.MergeOrUpload(documents);
+                await _indexClient.Documents.IndexAsync(batch);
+            }
+            else
+            {
+                var batch = IndexBatch.Upload(documents);
+                await _indexClient.Documents.IndexAsync(batch);
+            }
+        }
+
+        /// <summary>
+        /// Index documents.
+        /// </summary>
+        /// <param name="documents">The collection of documents.</param>
+        /// <param name="merge">If set to <c>true</c> merge otherwise just upload.</param>
+        /// <typeparam name="T">The document type.</typeparam>
+        public void IndexDocuments<T>(IEnumerable<T> documents, bool merge = true) where T: class
+        {
+            AsyncTools.RunSync(() => IndexDocumentAsync(documents, merge));
+        }
+
+        /// <summary>
+        /// Index documents.
+        /// </summary>
+        /// <param name="documents">The collection of documents.</param>
+        /// <param name="merge">If set to <c>true</c> merge otherwise just upload.</param>
+        /// <typeparam name="T">The document type.</typeparam>
+        public async Task IndexDocumentsAsync<T>(IEnumerable<T> documents, bool merge = true) where T: class
+        {
+            var chunks = documents.Select((x, i) => new { Index = i, Value = x }).GroupBy(x => x.Index / BatchSize).Select(x => x.Select(v => v.Value.MapToIndex()));
+
+            foreach(var chunk in chunks)
+            {
+                if (merge)
+                {
+                    var batch = IndexBatch.MergeOrUpload(chunk);
+                    await _indexClient.Documents.IndexAsync(batch);
+                }
+                else
+                {
+                    var batch = IndexBatch.MergeOrUpload(chunk);
+                    await _indexClient.Documents.IndexAsync(batch);
+                }
+            }
         }
 
         Index CreateIndex()
